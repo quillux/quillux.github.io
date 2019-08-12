7eedc07445ed6a90fb6cd59509f604fc5fe78398
james lofton <james.lofton@quill.com>
2019-08-12 08:07:32 -0500

    updated comments to be clearer
    +updated comments to be clearer
    +fixed problem with browsers now truncating the first level directory for some reason

--- js/rewrite-path_v2.js
+++ js/rewrite-path_v2.js
@@ -1,27 +1,27 @@
 
-// get local path to use and replace all instances
+// get local path for file that is loaded into browser to use and replace all instances
 var localFile = location.href;
 var arrPath = localFile.split("/");
 var arrLength = arrPath.length;
 
 var newPath = "";
 
-// remove the filename from the array and reassemble
+// remove the filename of existing file from the path array and reassemble
+// this builds the base for the file
 for( i=0; i<(arrLength-1); i++ ) {
   newPath += arrPath[i] + "/";
 }
-  
-// rewraite A tags that have "file:///locpath" as part of the link text
+
+// rewraite A tags that have "file:///" as part of the link text
 var arrAtags = document.getElementsByTagName("a");
  for ( z=0; z < arrAtags.length; z++ ) {
-     var rePath = arrAtags[z].href;           
-     arrAtags[z].href = rePath.replace('file:///locpath/', newPath);
+     var rePath = arrAtags[z].href;
+     arrAtags[z].href = rePath.replace('file:///', newPath);
  }
 
 // rewrite img tags that have "file:///locpath" as part of the src text
 var arrImgtags = document.getElementsByTagName("img");
  for ( x=0; x < arrImgtags.length; x++ ) {
      var reImgPath = arrImgtags[x].src;
-     arrImgtags[x].src = reImgPath.replace('file:///locpath/', newPath);
+     arrImgtags[x].src = reImgPath.replace('file:///', newPath);
  }
-       
--- js/rewrite-paths.js
+++ js/rewrite-paths.js
@@ -12,63 +12,19 @@ should make a version to rewrite image paths
 
 function getLocalPath (linkName) {
 
-     
-    // first we get the local path
-    var filePath = location.pathname;    
-    
+    // first we get the path of the file that called this function
+    var filePath = location.href;
+
     var arrPath = filePath.split("/");
     var arrLength = arrPath.length;
-    
+
     // get this file name
     var fileName = arrPath[arrLength - 1];
 
     // replace the file name with the file to link
     var newLink = filePath.replace(fileName, linkName);
-    
-    // need to determine platform and browser to parse the path properluy  
-    var stdPre = "file://";
-    // extra slash escapes the backslash for windows - there is only one slash = c:\
-    
 
-      if (detectIE()) {
-        // need to reverse the slashes and remove the first slash
-        newLink = newLink.replace(/\//g, '\\');
-        newLink = newLink.substr(1);
-        
-      } else {
-         // just append the std path prefix 
-         newLink = stdPre + newLink;
-        
-      }
-   
      // link to the new local location
-     window.location.href = newLink; 
+     window.location.href = newLink;
 
-}
-
-
-function detectIE() {
-  var ua = window.navigator.userAgent;
-
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    // IE 10 or older
-    return true;
-  }
-
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    // IE 11
-    var rv = ua.indexOf('rv:');
-    return true;
-  }
-
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    // Edge (IE 12+)
-    return true;
-  }
-
-  // other browser
-  return false;
 }
